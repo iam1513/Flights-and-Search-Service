e64a264aceb1fed381cb1f711ed7eac800f1afbe
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-11-19 13:13:19 +0530

    Added Bodyparser.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require("express")
-
+const bodyParser = require("body-parser")
 const {ServerConfig} = require('./config')
 
+// Create an express app
 const app = express();
 
-app.listen(ServerConfig.PORT, ()=>{
-    console.log(`Live on Server ${ServerConfig.PORT}`)
-})
+const StartTheServer = async ()=>{
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+
+    app.listen(ServerConfig.PORT, ()=>{
+        console.log(`Live on Server ${ServerConfig.PORT}`)
+    })
+}
+
+StartTheServer()
