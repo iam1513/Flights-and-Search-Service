660853f3e547f28b8f1f6e2d57765811a0a572b7
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-11-20 19:52:41 +0530

    Created our seeders file to seed Airport data. Added sync function.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const {ServerConfig} = require('./config')
 const apiRoutes = require('./routes/index')
+
+const db = require("./models/index")
+
 // Create an express app
 const app = express();
 
@@ -13,6 +16,11 @@ const StartTheServer = async ()=>{
     app.use('/api', apiRoutes);
 
     app.listen(ServerConfig.PORT, ()=>{
+
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter : true})
+        }
+
         console.log(`Live on Server ${ServerConfig.PORT}`)
     })
 }
